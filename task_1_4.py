54e01e6934a888c9cfa29a05a68917a6bef88fd2
Grigorash Pasha <GrigorashPasha@gmail.com>
2021-08-08 23:14:10 +0300

    2

---
+++ task_1_4.py
@@ -0,0 +1,11 @@
+import math
+"""
+the total variable shows the arithmetic average
+"""
+a = 7.5
+b = 8
+total = (a + b) / 2
+print(total)
+
+d = a * b
+print(math.sqrt(d))
---
+++ task_1_5.py
@@ -0,0 +1,8 @@
+a = 3
+b = 4
+c = (a ** 2) + (b ** 2)
+d = pow(c, 0.5)
+print(d)
+
+S = (a * b) * 0.5
+print(S)
